d6a6eb87281328aa0ac665c163e5ac24d3b0ee4e
Ovsjah <ovsjah@gmail.com>
2018-11-13 19:37:56 +0200

    Refactor seed.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-100.times do 
+123.times do 
   Product.create(
     name: Faker::Commerce.product_name,
     description: "Material - #{Faker::Commerce.material}. Color - #{Faker::Commerce.color}",
