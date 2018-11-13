fc1d096b727f72c46709a395eb87cc71a459c030
Ovsjah <ovsjah@gmail.com>
2018-11-13 19:21:44 +0200

    Create Product Seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+100.times do 
+  Product.create(
+    name: Faker::Commerce.product_name,
+    description: "Material - #{Faker::Commerce.material}. Color - #{Faker::Commerce.color}",
+    price: Faker::Commerce.price
+  )
+end
