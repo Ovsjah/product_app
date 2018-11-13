472e396727262f96b8cbc2e91f962ad1c2b63b7c
Ovsjah <ovsjah@gmail.com>
2018-11-13 19:03:01 +0200

    Add Product Search

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,13 @@
 class Product < ApplicationRecord
   validates :name, presence: true, length: { maximum: 50 }
   validates :description, presence: true, length: { maximum: 255 }
-  validates :price, presence: true, length: { maximum: 7 } 
+  validates :price, presence: true, length: { maximum: 7 }
+  
+  def self.search(search)
+    if search
+      where("lower(name || description) LIKE ?", "%#{search.downcase}%")
+    else
+      all
+    end
+  end
 end
